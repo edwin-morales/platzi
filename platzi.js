e167941d3ef41073d2a8340654e8316bd1ce3440
Edwin Morales <edwinmoralesv@outlook.com>
2022-05-18 11:26:40 -0500

    nuevo 5

---
+++ platzi.js
@@ -0,0 +1,58 @@
+var vp = document.getElementById("villaplatzi");
+var papel = vp.getContext("2d");
+
+var fondo = {
+   url: "tile.png",
+   cargaOK: false 
+}
+var vaca = {
+   url: "vaca.png",
+   cargaOK: false
+};
+ fondo.imagen = new Image();
+fondo.imagen.src = fondo.url;
+fondo.imagen.addEventListener("load", cargarFondo);
+
+vaca.imagen = new Image();
+vaca.imagen.src = vaca.url;
+vaca.imagen.addEventListener("load", cargarVaca);
+
+function cargarFondo()
+{
+fondo.cargaOK = true;
+dibujar();
+}
+
+function cargarVaca()
+{
+vaca.cargaOK = true;
+dibujar();
+}
+
+function dibujar()
+{
+   if(fondo.cargaOK)
+   {
+
+      papel.drawImage(fondo.imagen, 0, 0 )
+   }
+   if(vaca.cargaOK)
+   {
+      
+         for(var v=0; v < 10; v++)
+      {
+         var x = aleatorio(0, 420);
+         var y = aleatorio(0, 420);
+         papel.drawImage(vaca.imagen, x, y)
+      }
+     
+   }
+}
+
+function aleatorio(min, maxi)
+{
+    var resultado;
+    resultado = Math.floor(Math.random() * (maxi - min + 1)) + min;
+   return resultado;
+
+}
